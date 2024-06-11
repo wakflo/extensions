1d25ff1356ee8581ce9c80c64b12a80fd26b8b63
Rubicon Lee <132176609+raff-wakflo@users.noreply.github.com>
2024-06-11 17:07:18 -0500

    fix: updated sdk version
    Signed-off-by: Rubicon Lee <132176609+raff-wakflo@users.noreply.github.com>

--- internal/connectors/shopify/shared.go
+++ internal/connectors/shopify/shared.go
@@ -10,23 +10,21 @@ import (
 	{Const: "custom", Title: "Shopify (Custom/Private)"},
 }*/
 
-var (
-	sharedAuth = autoform.NewCustomAuthField().
-		SetFields(map[string]*sdkcore.AutoFormSchema{
-			"domain": autoform.NewShortTextField().SetDisplayName("Domain Name").
-				SetDescription("The domain name of the shopify app.").
-				SetRequired(true).
-				Build(),
-			"token": autoform.NewShortTextField().SetDisplayName("Authentication Token").
-				SetDescription("The token used to authenticate the shopify app.").
-				Build(),
-			/*// will be enabled when dropdown cab show in dialog
-			// "appMode": autoform.NewSelectField().
-			//	SetDisplayName("Application Mode").
-			//	SetOptions(viewStyleOptions).
-			//	SetRequired(true).
-			//	SetDescription("The application mode of the shopify app.").
-			//	Build(),*/
-		}).
-		Build()
-)
+var sharedAuth = autoform.NewCustomAuthField().
+	SetFields(map[string]*sdkcore.AutoFormSchema{
+		"domain": autoform.NewShortTextField().SetDisplayName("Domain Name").
+			SetDescription("The domain name of the shopify app.").
+			SetRequired(true).
+			Build(),
+		"token": autoform.NewShortTextField().SetDisplayName("Authentication Token").
+			SetDescription("The token used to authenticate the shopify app.").
+			Build(),
+		/*// will be enabled when dropdown cab show in dialog
+		// "appMode": autoform.NewSelectField().
+		//	SetDisplayName("Application Mode").
+		//	SetOptions(viewStyleOptions).
+		//	SetRequired(true).
+		//	SetDescription("The application mode of the shopify app.").
+		//	Build(),*/
+	}).
+	Build()
