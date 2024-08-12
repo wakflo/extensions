872c810e4b8ca110f9eba278f4f10b689f73a2ec
Rubicon Lee <132176609+raff-wakflo@users.noreply.github.com>
2024-08-12 14:50:10 -0500

    fix: added google-drive docs setup
    Signed-off-by: Rubicon Lee <132176609+raff-wakflo@users.noreply.github.com>

--- internal/connectors/googlemail/lib.go
+++ internal/connectors/googlemail/lib.go
@@ -1,17 +1,23 @@
 package googlemail
 
 import (
+	_ "embed"
+
 	sdk "github.com/wakflo/go-sdk/connector"
 )
 
+//go:embed docs.mdx
+var doc string
+
 func NewConnector() (*sdk.ConnectorPlugin, error) {
 	return sdk.CreateConnector(&sdk.CreateConnectorArgs{
-		Name:        "Google Mail",
-		Description: " Connect to Google Mail to read, send and manage emails",
-		Logo:        "logos:google-gmail",
-		Version:     "0.0.1",
-		Category:    sdk.Apps,
-		Authors:     []string{"Wakflo <integrations@wakflo.com>"},
+		Name:          "Google Mail",
+		Description:   "Connect to Google Mail to read, send and manage emails",
+		Logo:          "logos:google-gmail",
+		Version:       "0.0.1",
+		Category:      sdk.Apps,
+		Authors:       []string{"Wakflo <integrations@wakflo.com>"},
+		Documentation: doc,
 		Triggers: []sdk.ITrigger{
 			NewTriggerNewEmail(),
 		},
