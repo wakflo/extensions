eb9c9cd78a7a671088377a1f1813a2ca8ceb52d0
whizzie99 <wisdomobiomadev@gmail.com>
2024-09-28 01:43:38 +0100

    create calendly connector and operations

--- internal/connectors/calendly/shared.go
+++ internal/connectors/calendly/shared.go
@@ -3,33 +3,16 @@ package calendly
 import (
 	"encoding/json"
 	"errors"
-	"io"
-	"net/http"
-
+	"fmt"
+	"github.com/gookit/goutil/arrutil"
 	fastshot "github.com/opus-domini/fast-shot"
 	"github.com/wakflo/go-sdk/autoform"
+	sdk "github.com/wakflo/go-sdk/connector"
 	sdkcore "github.com/wakflo/go-sdk/core"
+	"io"
+	"net/http"
+	"regexp"
 )
-
-type User struct {
-	URI                 string `json:"uri"`
-	Name                string `json:"name"`
-	Slug                string `json:"slug"`
-	Email               string `json:"email"`
-	SchedulingURL       string `json:"scheduling_url"`
-	Timezone            string `json:"timezone"`
-	CreatedAt           string `json:"created_at"`
-	UpdatedAt           string `json:"updated_at"`
-	CurrentOrganization string `json:"current_organization"`
-	AvatarURL           string `json:"avatar_url"`
-}
-type UsersResponse struct {
-	Users []User `json:"collection"`
-}
-
-type CurrentUserResponse struct {
-	Resource User `json:"resource"`
-}
 
 var (
 	// #nosec
@@ -38,54 +21,6 @@ var (
 )
 
 const baseURL = "https://api.calendly.com"
-
-// func getCalendlyUsersInput(title string, desc string, required bool) *sdkcore.AutoFormSchema {
-// 	getUsers := func(ctx *sdkcore.DynamicFieldContext) (interface{}, error) {
-// 		client := fastshot.NewClient(baseURL).
-// 			Auth().BearerToken(ctx.Auth.AccessToken).
-// 			Header().
-// 			AddAccept("application/json").
-// 			Build()
-
-// 		rsp, err := client.GET("/users").Send()
-// 		if err != nil {
-// 			return nil, err
-// 		}
-
-// 		defer rsp.Body().Close()
-
-// 		if rsp.Status().IsError() {
-// 			return nil, errors.New(rsp.Status().Text())
-// 		}
-
-// 		byts, err := io.ReadAll(rsp.Body().Raw())
-// 		if err != nil {
-// 			return nil, err
-// 		}
-
-// 		var body UsersResponse
-
-// 		err = json.Unmarshal(byts, &body)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-
-// 		users := body.Users
-
-// 		return arrutil.Map[User, map[string]any](users, func(input User) (target map[string]any, find bool) {
-// 			return map[string]any{
-// 				"id":   input.URI,
-// 				"name": input.Name,
-// 			}, true
-// 		}), nil
-// 	}
-
-// 	return autoform.NewDynamicField(sdkcore.String).
-// 		SetDisplayName(title).
-// 		SetDescription(desc).
-// 		SetDynamicOptions(&getUsers).
-// 		SetRequired(required).Build()
-// }
 
 func getCurrentCalendlyUserInput(title string, desc string, required bool) *sdkcore.AutoFormSchema {
 	getCurrentUser := func(ctx *sdkcore.DynamicFieldContext) (interface{}, error) {
@@ -135,6 +70,63 @@ func getCurrentCalendlyUserInput(title string, desc string, required bool) *sdkc
 		SetRequired(required).Build()
 }
 
+func getCalendlyEventInput(title string, desc string, required bool) *sdkcore.AutoFormSchema {
+	getEvents := func(ctx *sdkcore.DynamicFieldContext) (interface{}, error) {
+		input := sdk.DynamicInputToType[struct {
+			User string `json:"user"`
+		}](ctx)
+
+		client := fastshot.NewClient(baseURL).
+			Auth().BearerToken(ctx.Auth.AccessToken).
+			Header().
+			AddAccept("application/json").
+			Build()
+
+		req := client.GET("/scheduled_events")
+		req.Query().AddParam("user", input.User)
+
+		rsp, err := req.Send()
+		if err != nil {
+			return nil, err
+		}
+
+		defer rsp.Body().Close()
+
+		if rsp.Status().IsError() {
+			return nil, errors.New(rsp.Status().Text())
+		}
+
+		byts, err := io.ReadAll(rsp.Body().Raw())
+		if err != nil {
+			return nil, err
+		}
+
+		var body EventsResponse
+
+		err = json.Unmarshal(byts, &body)
+		if err != nil {
+			return nil, err
+		}
+		events := body.Events
+
+		return arrutil.Map[Event, map[string]any](events, func(input Event) (target map[string]any, find bool) {
+			return map[string]any{
+				"id":    input.URI,
+				"name":  input.Name,
+				"start": input.StartTime,
+				"end":   input.EndTime,
+			}, true
+		}), nil
+	}
+
+	return autoform.NewDynamicField(sdkcore.String).
+		SetDisplayName(title).
+		SetDescription(desc).
+		SetDynamicOptions(&getEvents).
+		SetRequired(required).
+		Build()
+}
+
 func listEvents(accessToken, url string, status string, user string) (map[string]interface{}, error) {
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
@@ -163,7 +155,51 @@ func listEvents(accessToken, url string, status string, user string) (map[string
 	return response, nil
 }
 
+func getEvent(accessToken, eventId string) (map[string]interface{}, error) {
+	url := baseURL + "/scheduled_events/" + getEventID(eventId)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+accessToken)
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %w", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("API returned non-OK status: %s", res.Status)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	var response map[string]interface{}
+	if err := json.Unmarshal(body, &response); err != nil {
+		return nil, fmt.Errorf("error decoding response: %w", err)
+	}
+
+	return response, nil
+}
+
 var calendlyEventStatusType = []*sdkcore.AutoFormSchema{
 	{Const: "active", Title: "Active"},
 	{Const: "canceled", Title: "Canceled"},
+}
+
+func getEventID(url string) string {
+	// Define the regex pattern to capture the alphanumeric string after scheduled_events/
+	re := regexp.MustCompile(`scheduled_events/([a-zA-Z0-9-]+)$`)
+	match := re.FindStringSubmatch(url)
+
+	// Check if there is a match
+	if len(match) > 1 {
+		return match[1]
+	}
+	return ""
 }
