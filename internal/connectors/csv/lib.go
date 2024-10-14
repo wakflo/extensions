f74d5470dde6099bbd9be435de9e0d365d4972fb
Rubicon Lee <rex@wakflo.com>
2024-10-14 12:04:58 -0500

    fix: fixed test
    Signed-off-by: Rubicon Lee <rex@wakflo.com>

--- internal/connectors/csv/lib.go
+++ internal/connectors/csv/lib.go
@@ -24,7 +24,7 @@ func NewConnector() (*sdk.ConnectorPlugin, error) {
 		Description: "csv reader utils and functions",
 		Logo:        "iwwa:file-csv",
 		Version:     "0.0.1",
-		Category:    sdk.Tools,
+		Category:    sdk.Core,
 		Authors:     []string{"Wakflo <integrations@wakflo.com>"},
 		Triggers:    []sdk.ITrigger{},
 		Operations: []sdk.IOperation{
